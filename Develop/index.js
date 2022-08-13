1d7f4d7475b081ae444d23220333a8a2824d2cd2
aurumbright <aurum.elean@gmail.com>
2022-08-13 10:28:29 -0700

    inquirer questions and filewriter

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,80 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+// this links the generateMarkdown script
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please describe this project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter the installation instructions.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter the usage instructions.',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter the contribution guidelines.',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter the test instructions.',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose the license for your application.',
+        choices: [
+            "MIT",
+            "Apache 2.0",
+            "GNU GPLv3",
+            "ISC License",
+            "Mozilla Public License 2.0",
+            "Boost Software License 1.0",
+            "The Unilicense",
+        ]
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address so people can contact you with questions about the app.',
+    },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function runs questions and writes README file upon answer return
+function init() {
+    return inquirer
+        .prompt(questions)
+        // prompt returns a promise - passed as answers
+        .then((answers) => {
+            const readMeFile = generateMarkdown(answers);
 
-// TODO: Create a function to initialize app
-function init() {}
+            fs.writeFile('README.md', readMeFile, (err) =>
+                err ? console.log(err) : console.log('Successfully created README!')
+            );
+        });
+}
 
 // Function call to initialize app
 init();
+
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,55 @@
+// This script should be the Markdown file structure
+// It's broken down into all the license stuff
+// and then the Markdown file itself
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.name}
+  ## Description
+    ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+    ${data.installation}
+
+  ## Usage
+    ${data.usage}
+
+  ## License
+
+
+  ## Contributing
+    ${data.contributing}
+  
+  ## Tests
+    ${data.tests}
+
+  ## Questions
+    ${data.email}
+    ${data.github}
 
 `;
 }
